387adbee6522b03a6d3c785236f4d3f6bcb07c44
tsur0evv <storm00606@gmail.com>
2025-12-24 10:27:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
-const fs = require('fs')
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
 
 const parse = async () => {
-  const getHTML = async (url) => {
-    const { data } = await axios.get(url);
-    return cheerio.load(data);
-  };
 
-  const $ = await getHTML("https://kanobu.ru/games/playstation_5/popular/");
-  const pageNumber = $("a.ui-kit-paginator--list-link").eq(-2).text();
-  console.log(pageNumber);
+    const getHTML = async (url) => {
+        const { data } = await axios.get(url);
+        return cheerio.load(data);
+    }
 
-  for (let i = 1; i <= pageNumber; i++) {
-    const selector = await getHTML(
-      `https://kanobu.ru/games/playstation_5/popular/?page=${i}`
-    );
-    selector(".c-game").each((i, element) => {
-      const title = selector(element).find("div.h2").text();
-      const link = `https://kanobu.ru${selector(element)
-        .find("a")
-        .attr("href")}`;
-      
-        
-        fs.appendFileSync('./data.txt', `${title}; ${link}\n`)
-    });
-  }
-};
+    const $ = await getHTML('https://byrutgame.org/top-torrent-games/')
+    const pageNumber = $('div.pages > a').eq(-2).text()
 
-parse();
+    for (let i = 1; i < pageNumber; i++) {
+        const selector = await getHTML(`https://byrutgame.org/top-torrent-games/page/${i}/`)
+
+        selector('.short_item').each((i, element) => {
+            const title = selector(element).find('div.short_title').text();
+            const link = selector(element).find('a').attr('href');
+
+            fs.appendFileSync('./data.txt', `${title}${link}\n`);
+        })
+    }
+
+}
+
+parse()
